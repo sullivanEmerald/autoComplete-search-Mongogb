b7aa2104dc2f5c5104cfdb23dd24f62ba1f8d2f4
Sullivan Amadike <amadikesullivan20@gmail.com>
2022-06-22 15:15:12 +0100

    searchig movies

--- server.js
+++ server.js
@@ -1,59 +1,82 @@
 const express = require('express')
 const app = express()
-const {MongoClient, ObjectId} = require('mongodb')
-const PORT =  process.env.PORT || 7000
 const cors = require('cors')
+const {MongoClient, ObjectId } = require('mongodb')
+const { response } = require('express')
+const { request } = require('http')
 require('dotenv').config()
+const PORT = 8000
 
-let db, 
-    connetionString  = process.env.DB_STRING,
+let db,
+    dbConnectionStr = process.env.DB_STRING,
     dbName = 'sample_mflix',
     collection
 
-MongoClient.connect(connetionString)
+MongoClient.connect(dbConnectionStr)
     .then(client => {
-        console.log(`connected to ${dbName} Database`)
+        console.log(`Connected to database`)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
-app.use(cors())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended : true }))
-app.use(express.json())
 
-app.get('/search', async (request, response) => {
-    try{
-        let result =  await collection.aggregate([
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+app.use(cors())
+
+app.get("/search", async (request,response) => {
+    try {
+        let result = await collection.aggregate([
             {
                 "$search" : {
                     "autocomplete" : {
-                        "query" : `${request.query.query}`,
-                        "path" : "title",
-                        "fuzzy" : {
-                            "MaxEdits" : 2,
-                            "prefixLength" : 3
+                        "query": `${request.query.query}`,
+                        "path": "title",
+                        "fuzzy": {
+                            "maxEdits":2,
+                            "prefixLength": 3
                         }
                     }
                 }
             }
         ]).toArray()
+        //console.log(result)
         response.send(result)
-    }catch(error){
-       response.status(404).send({message : error.message})
+    } catch (error) {
+        response.status(500).send({message: error.message})
+        //console.log(error)
     }
 })
 
-app.get('/get/:id', async (request, response) => {
-    try{
-        let result =  await collection.findOne({
+app.get("/get/:id", async (request, response) => {
+    try {
+        let result = await collection.findOne({
             "_id" : ObjectId(request.params.id)
         })
         response.send(result)
-    }catch(error){
-        response.status(404).send({message : error.message})
+    } catch (error) {
+        response.status(500).send({message: error.message})
     }
+}
+)
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running.`)
 })
 
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT} local Server`)
-})
+//THIS IS THE INDEX TO APPLY TO MONGODB MOVIES COLLECTION
+// {
+//     "mappings": {
+//         "dynamic": false,
+//         "fields": {
+//             "title": [
+//                 {
+//                     "foldDiacritics": false,
+//                     "maxGrams": 7,
+//                     "minGrams": 3,
+//                     "tokenization": "edgeGram",
+//                     "type": "autocomplete"
+//                 }
+//             ]
+//         }
+//     }
+// }
